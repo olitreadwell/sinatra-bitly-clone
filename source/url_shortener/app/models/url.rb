f3001bb3f67d82f0e9d62e6aad4a8c20152013e8
Apprentice <apprentice@dbc-chicago-009.local>
2014-05-13 11:30:48 -0500

    URL and custom URL now have validations in place for users.
    Finished up validations and callbacks in our URL model.

--- source/url_shortener/app/models/url.rb
+++ source/url_shortener/app/models/url.rb
@@ -5,9 +5,10 @@ class Url < ActiveRecord::Base
     self.visit_count ||= 0
   end
 
-  validates :resource, presence: true
+  validates :resource, presence: true,
+                       format: { with: /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/,
+                                 message: "Not a valid URL"}
   validates :short_resource, uniqueness: true
-
 
 end
 
