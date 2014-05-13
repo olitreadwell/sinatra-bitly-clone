7fe5cfb7ec6b72f75a9472fcc2ae3c0056c2d3c7
Apprentice <apprentice@dbc-chicago-009.local>
2014-05-13 14:43:22 -0500

    Provide logic for creating short_urls Randomized end of short url Created Getter and Setter Methods

--- source/url_shortener/app/controllers/index.rb
+++ source/url_shortener/app/controllers/index.rb
@@ -1,4 +1,17 @@
 get '/' do
   # Look in app/views/index.erb
+  # let user create new short URL, display a list of shortened URLs
   erb :index
+end
+
+post '/urls' do
+  @url = Url.create(resource: params[:resource])
+  redirect to "/"
+end
+
+# e.g., /q6bda
+get '/:short_url' do
+  @url = Url.find_by_short_resource(params[:short_resource])
+  # redirect to appropriate "long" URL
+  redirect to "#{@url.resource}"
 end
--- source/url_shortener/app/models/url.rb
+++ source/url_shortener/app/models/url.rb
@@ -5,6 +5,21 @@ class Url < ActiveRecord::Base
     self.visit_count ||= 0
   end
 
+  before_save :set_short_url # changed to this from :create_short_url
+
+  def create_short_url
+    #method that will generate a "random" URL
+    only_letters = (self.resource).chars.select { |char| char =~ /[a-zA-Z]/ }
+    number_array = (0..9).to_a.sample(only_letters.length)
+    combined_array = only_letters + number_array
+    end_of_short_url = combined_array.sample(7).join("")
+    return "bit.oli/#{end_of_short_url}"
+  end
+
+  def set_short_url
+    self.short_resource = create_short_url
+  end
+
   validates :resource, presence: true,
                        format: { with: /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/,
                                  message: "Not a valid URL"}
